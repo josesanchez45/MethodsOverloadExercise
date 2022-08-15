dbe9503a8e9494fe48d4d3aa1e1af727c6ccd343
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-15 12:38:33 -0700

    first commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static void Add(int num1, int num2, bool trueOrFalse)
+        {
+            var sum = num1 + num2;
+            if (trueOrFalse && sum == 0)
+            {
+                Console.WriteLine($"{sum} means you are broke");
+            }
+            else if (trueOrFalse == true && sum == 1)
+            {
+                Console.WriteLine($" Congrats on {sum} dollar");
+            }
+            else
+            {
+                Console.WriteLine($"{sum} dollars is more than 1");
+            }
+        }
+    }
+}
+ 
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,5 @@
+﻿using MethodOverloading;
+
+Console.WriteLine(Methods.Add(5, 6));
+Console.WriteLine (Methods.Add(5.5m, 7.5m));
+Methods.Add(0, 1, true);
